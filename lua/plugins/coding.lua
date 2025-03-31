e9335feb94398374fb8c726fd782d528430fbf55
Dung Huynh Duc <dunghd.it@gmail.com>
2025-03-31 17:45:16 +0800

    feat(refactoring): add key mappings and options for refactoring and debugging

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -184,4 +184,166 @@ return {
       },
     },
   },
+  -- Refactoring
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>d", group = "debug" },
+        { "<leader>r", group = "refactoring", icon = "" },
+      },
+    },
+  },
+  -- The Refactoring library based off the Refactoring book by Martin Fowler
+  {
+    "ThePrimeagen/refactoring.nvim",
+    vscode = true,
+    dependencies = {
+      { "nvim-lua/plenary.nvim", vscode = true },
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+    keys = {
+      {
+        "<leader>rm",
+        function()
+          require("refactoring").select_refactor {
+            show_success_message = true,
+          }
+        end,
+        mode = { "n", "v" },
+        desc = "Refactoring Menu",
+      },
+      {
+        "<leader>re",
+        function()
+          require("refactoring").refactor "Extract Function"
+        end,
+        desc = "Extract",
+        mode = "x",
+      },
+      {
+        "<leader>rf",
+        function()
+          require("refactoring").refactor "Extract Function To File"
+        end,
+        desc = "Extract to file",
+        mode = "x",
+      },
+      {
+        "<leader>rv",
+        function()
+          require("refactoring").refactor "Extract Variable"
+        end,
+        desc = "Extract variable",
+        mode = "x",
+      },
+      {
+        "<leader>ri",
+        function()
+          require("refactoring").refactor "Inline Variable"
+        end,
+        desc = "Inline variable",
+        mode = { "n", "x" },
+      },
+      {
+        "<leader>rI",
+        function()
+          require("refactoring").refactor "Inline Function"
+        end,
+        desc = "Inline function",
+        mode = { "n" },
+      },
+      {
+        "<leader>rb",
+        function()
+          require("refactoring").refactor "Extract Block"
+        end,
+        desc = "Extract block",
+      },
+      {
+        "<leader>rB",
+        function()
+          require("refactoring").refactor "Extract Block To File"
+        end,
+        desc = "Extract block to file",
+      },
+      -- Debug variable
+      {
+        "<leader>dv",
+        function()
+          require("refactoring").debug.print_var {
+            show_success_message = true,
+            below = true,
+          }
+        end,
+        mode = { "n", "x" },
+        desc = "Print below variables",
+      },
+      {
+        "<leader>dV",
+        function()
+          require("refactoring").debug.print_var {
+            show_success_message = true,
+            below = false,
+          }
+        end,
+        mode = { "n", "x" },
+        desc = "Print above variables",
+      },
+      -- Clean up debugging
+      {
+        "<leader>dc",
+        function()
+          require("refactoring").debug.cleanup {
+            force = true,
+            show_success_message = true,
+          }
+        end,
+        desc = "Clear debugging",
+      },
+    },
+    opts = {
+      prompt_func_return_type = {
+        go = false,
+        java = false,
+
+        cpp = false,
+        c = false,
+        h = false,
+        hpp = false,
+        cxx = false,
+      },
+      prompt_func_param_type = {
+        go = false,
+        java = false,
+
+        cpp = false,
+        c = false,
+        h = false,
+        hpp = false,
+        cxx = false,
+      },
+      printf_statements = {},
+      print_var_statements = {},
+    },
+  },
+  -- Code comment
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+  },
+  {
+    -- A better annotation generator. Supports multiple languages and annotation conventions.
+    -- <C-n> to jump to next annotation, <C-p> to jump to previous annotation
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    opts = { enabled = true },
+    cmd = "Neogen",
+    vscode = true,
+    keys = {
+      { "<leader>ci", "<cmd>Neogen<cr>", desc = "Neogen: Annotation generator" },
+    },
+  },
 }
