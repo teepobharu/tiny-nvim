b32c2ab6abf70ee1ccec0ec8f67367702aca4b05
Dung Huynh Duc <dunghd.it@gmail.com>
2025-03-31 17:44:29 +0800

    fix(theme): invert condition for loading theme and VSCode keymaps

--- init.lua
+++ init.lua
@@ -4,22 +4,22 @@ require "config.lazy"
 require "config.keymaps"
 
 -- Only load the theme if not in VSCode
-if not vim.g.vscode then
-  require("kanagawa").load "wave"
-else
+if vim.g.vscode then
   -- Trigger vscode keymap
   local pattern = "NvimIdeKeymaps"
   vim.api.nvim_exec_autocmds("User", { pattern = pattern, modeline = false })
+else
+  -- Load the theme
+  require("kanagawa").load "wave"
+  -- Enable LSP servers for Neovim 0.11+
+  vim.lsp.enable {
+    "luals", -- Lua
+    "tsls", -- or "vtsls" for TypeScript
+    "biome", -- Biome = Eslint + Prettier but not fully yet, v2 beta
+    "eslint", -- Linter
+    "json", -- JSON
+    "pyright", -- Python
+    "gopls", -- Go
+    "tailwindcss", -- Tailwind CSS
+  }
 end
-
--- Enable LSP servers for Neovim 0.11+
-vim.lsp.enable {
-  "luals", -- Lua
-  "tsls", -- or "vtsls" for TypeScript
-  "biome", -- Biome = Eslint + Prettier but not fully yet, v2 beta
-  "eslint", -- Linter
-  "json", -- JSON
-  "pyright", -- Python
-  "gopls", -- Go
-  "tailwindcss", -- Tailwind CSS
-}
