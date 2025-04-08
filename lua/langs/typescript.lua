95f2859dadb8905fbfc7d90ce01392283e0a3e96
Dung Huynh Duc <dunghd.it@gmail.com>
2025-04-08 17:16:41 +0800

    feat(typescript): add typecheck.nvim plugin for type checking
    refactor: simplify nativeMCPServers structure in mcphub.json

--- lua/langs/typescript.lua
+++ lua/langs/typescript.lua
@@ -3,4 +3,18 @@ return {
     "dmmulroy/ts-error-translator.nvim",
     ft = "javascript,typescript,typescriptreact,svelte",
   },
+  {
+    "jellydn/typecheck.nvim",
+    ft = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" },
+    opts = {
+      debug = true,
+    },
+    keys = {
+      {
+        "<leader>ck",
+        "<cmd>Typecheck<cr>",
+        desc = "Run Type Check",
+      },
+    },
+  },
 }
