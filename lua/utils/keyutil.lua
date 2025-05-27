7ed5a61de14b26df75e42a707ff4a3866e0b98b8
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2025-05-27 12:55:52 +0700

    feat(tmux): add tmux navigator plugin
    feat(keymaps): Remove window navigation keymaps for tmux

--- lua/utils/keyutil.lua
+++ lua/utils/keyutil.lua
@@ -2,7 +2,6 @@ local M = {}
 
 M.isSnackEnabled = vim.g.enable_extra_plugins and vim.tbl_contains(vim.g.enable_extra_plugins, "snacks") or true
 M.isToggleTermEnabled = vim.g.enable_extra_plugins and vim.tbl_contains(vim.g.enable_extra_plugins, "myToggleterm")
-print([==[ M.isSnackEnabled:]==], vim.inspect(M.isSnackEnabled)) -- __AUTO_GENERATED_PRINT_VAR_END__
 
 local function get_prefix_key(key)
   if M.isSnackEnabled then
