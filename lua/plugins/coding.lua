f920307b81fe6f98f68a31050d55d6acf854dbd1
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2025-05-30 01:31:49 +0700

    fix(codecompanion): blink enable for codecompanion cmp disable conflict 🔥

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -120,7 +120,6 @@ return {
       -- Disable per file type
       enabled = function()
         return not vim.tbl_contains({ "copilot-chat" }, vim.bo.filetype)
-          and not vim.tbl_contains({ "codecompanion" }, vim.bo.filetype)
           and vim.bo.buftype ~= "prompt"
           and vim.b.completion ~= false
       end,
