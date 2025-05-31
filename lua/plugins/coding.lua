d84b039c4f3268e4cd961a28f975debba820a3ec
Teepob Harutaipree <Teepob.Harutaipree@agoda.com>
2025-05-31 15:01:52 +0700

    feat(coding): Disable copilot as default source in blink-cmp, add c-c key to trigger manual

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -95,8 +95,8 @@ return {
       -- Disable per file type
       enabled = function()
         return not vim.tbl_contains({ "copilot-chat" }, vim.bo.filetype)
-          and vim.bo.buftype ~= "prompt"
-          and vim.b.completion ~= false
+            and vim.bo.buftype ~= "prompt"
+            and vim.b.completion ~= false
       end,
     },
     -- without having to redefine it
@@ -155,6 +155,7 @@ return {
       },
     },
   },
+
   -- Refactoring
   {
     "folke/which-key.nvim",
@@ -171,7 +172,7 @@ return {
     "ThePrimeagen/refactoring.nvim",
     vscode = true,
     dependencies = {
-      { "nvim-lua/plenary.nvim", vscode = true },
+      { "nvim-lua/plenary.nvim",          vscode = true },
       { "nvim-treesitter/nvim-treesitter" },
     },
     keys = {
@@ -326,14 +327,14 @@ return {
             i = { "@block.inner", "@conditional.inner", "@loop.inner" },
           },
           f = ai.gen_spec.treesitter { a = "@function.outer", i = "@function.inner" }, -- function
-          c = ai.gen_spec.treesitter { a = "@class.outer", i = "@class.inner" }, -- class
-          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
-          d = { "%f[%d]%d+" }, -- digits
-          e = { -- Word with case
+          c = ai.gen_spec.treesitter { a = "@class.outer", i = "@class.inner" },       -- class
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },          -- tags
+          d = { "%f[%d]%d+" },                                                         -- digits
+          e = {                                                                        -- Word with case
             { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
             "^().*()$",
           },
-          u = ai.gen_spec.function_call(), -- u for "Usage"
+          u = ai.gen_spec.function_call(),                          -- u for "Usage"
           U = ai.gen_spec.function_call { name_pattern = "[%w_]" }, -- without dot in function name
         },
       }
